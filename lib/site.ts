40e4a6e3e093b57a4760a87e49de881c9e90ba45
vandanavnaik66 <vandanavnaik66@gmail.com>
2026-01-18 19:18:49 +0530

    resume-updation

--- lib/site.ts
+++ lib/site.ts
@@ -3,7 +3,7 @@ export const site = {
   name: "Vandana Naik",
   description: "A calm, Apple-inspired portfolio built with Next.js.",
   resumeUrl:
-    "https://drive.google.com/file/d/1La40Cjyso3jI0SDiuWa5mWBjbohgpgRm/view?usp=sharing",
+    "https://drive.google.com/file/d/11EtnDbzxeaynJESjRKzuFgg67ZFjSNkc/view?usp=sharing",
   contactEmailTo: process.env.CONTACT_TO_EMAIL ?? "vandanavnaik66@gmail.com",
   socials: {
     github: "https://github.com/vandanavnaik66",
